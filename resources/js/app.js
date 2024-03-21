1f363c567eb607450e0db30be6f0f7dc7af34580
eko-star <ekinkaplan444@gmail.com>
2024-03-21 18:20:45 +0300

    vue ile form verileri post edildi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import App from "./components/App.vue"; // Değişiklik burada
+
+const app = createApp({
+    components: {
+        App
+    },
+    template: '<App/>' // Değişiklik burada
+});
+
+app.mount('#app');
+window.axios = require('axios');
